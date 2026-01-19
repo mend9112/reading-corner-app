ad94c991b6759394a2ecd8c87cb62edf7d0db185
marisa <mend9112@pacificu.edu>
2026-01-19 14:54:50 -0800

    Adding Color Scheme Logic and _layout page

---
+++ app/_layout.jsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, useColorScheme, View } from 'react-native'
+import { Slot, Stack } from 'expo-router'
+import { Colors } from '../constants/Colors'
+
+const RootLayout = () => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light;
+
+  return (
+    // <Stack screenOptions={ {
+    //   headerStyle: { backgroundColor: theme.background },
+    //   headerTitleStyle: { color: theme.text },
+    //   headerTintColor: theme.text,
+    //   contentStyle: { backgroundColor: theme.background }
+    // } }>
+    //   <Stack.Screen name="index" options={ {headerShown: false} }/>
+    //   <Stack.Screen name="welcome2" options={ {headerShown: false} }/>
+    //   <Stack.Screen name="welcome3" options={ {headerShown: false} }/>
+    //   <Stack.Screen name="loginLanding" options={ {headerShown: false} }/>
+    //   <Stack.Screen name="signIn" options={ {headerShown: false} }/>
+    //   <Stack.Screen name="createAccount" options={ {headerShown: false} }/>
+    // </Stack>
+    <View><Text>Hello</Text></View>
+  )
+}
+
+export default RootLayout
+
+const styles = StyleSheet.create({})
---
+++ constants/Colors.js
@@ -0,0 +1,32 @@
+const colorsRC = {
+  /* Primary */
+  white: '#FFFFFF',
+  cream: '#ECE7DF',
+  forest: '#505535',
+  caramel: '#C67D43',
+  ink: '#1E1E0D',
+
+  /* Secondary */
+  offWhite: '#F7F5EF',
+  beige: '#D3CABA',
+  sage: '#AEB8A4',
+  pine: '#282D0D',
+  copper: '#9E553A',
+};
+
+export const Colors = {
+  dark: {
+    text: colorsRC.cream,
+    title: colorsRC.sage,
+    background: colorsRC.ink,
+    navBackground: colorsRC.forest,
+    iconColor: colorsRC.cream,
+  },
+  light: {
+    text: colorsRC.ink,
+    title: colorsRC.sage,
+    background: colorsRC.offWhite,
+    navBackground: colorsRC.forest,
+    iconColor: colorsRC.cream,
+  },
+}
